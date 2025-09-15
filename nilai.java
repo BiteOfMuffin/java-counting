5827e2d820545bbfcf860a31ef9ee5dfff724239
Muffinz... <kanandaarjun@gmail.com>
2025-09-15 13:51:59 +0800

    Create nilai.java

---
+++ nilai.java
@@ -0,0 +1,15 @@
+public class nilai {
+    public static void main(String[] args) {
+        int nilai1 = 85;
+        int nilai2 = 90;
+        int nilai3 = 75;
+
+        if(nilai1 >= nilai2 && nilai1 >= nilai3) {
+            System.out.println("Nilai tertinggi adalah: " + nilai1);
+        } else if(nilai2 >= nilai1 && nilai2 >= nilai3) {
+            System.out.println("Nilai tertinggi adalah: " + nilai2);
+        } else {
+            System.out.println("Nilai tertinggi adalah: " + nilai3);
+        }
+    }
+}
