7a9983a8d19d9641eba6c06a10a0542e8ec3d1be
Muffinz... <kanandaarjun@gmail.com>
2025-09-15 16:24:08 +0800

    Update nilai.java

--- nilai.java
+++ nilai.java
@@ -2,7 +2,7 @@ public class nilai {
     public static void main(String[] args) {
         int nilai1 = 85;
         int nilai2 = 90;
-        int nilai3 = 75;
+        int nilai3 = 35;
 
         if(nilai1 >= nilai2 && nilai1 >= nilai3) {
             System.out.println("Nilai tertinggi adalah: " + nilai1);
