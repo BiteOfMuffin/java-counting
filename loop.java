60682d577c139cd7ee12657f89ce2c70f18afe3b
Muffinz... <kanandaarjun@gmail.com>
2025-09-22 12:50:43 +0800

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,7 @@
+public class loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i += 2) {
+            System.out.println("Kelipatan 3: " + i);
+        }
+    }
+}
